38c9ce979f651e4b699fecd4737a66c3b73f6cc4
AR <dostoevskijdinozavr@gmail.com>
2024-09-22 12:59:31 +0700

    Added class ReadFromFile to process data from a filе

--- MeterReading/MeterReading/Program.cs
+++ MeterReading/MeterReading/Program.cs
@@ -11,7 +11,7 @@ namespace MeterReading
         
         static void Main(string[] args)
         {
-           
+            Console.WriteLine(ReadFromFile.ReadFile());
         }
     }
 }
---
+++ MeterReading/MeterReading/ReadFromFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeterReading
+{
+    internal class ReadFromFile
+    {
+        static string filePath = "C:\\Users\\user\\Desktop\\MeterReading\\MeterReading\\MeterReading\\Data.txt";
+
+        public static string ReadFile()
+        {
+            string[] MetersData = File.ReadAllLines(filePath);
+            string output = "";
+            foreach (string MeterData in MetersData)
+            {
+                StringToMeter.TrySetMeterData(MeterData);
+                output += StringToMeter.GetMeterData() + "\n";
+            }
+            return output;
+        }
+    }
+}
